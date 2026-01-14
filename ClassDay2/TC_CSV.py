77c710c5c1ff5c707beb822609f53c6f438cfc1a
MahendraChowdam <135975490+MahendraChowdam@users.noreply.github.com>
2026-01-14 14:37:35 +0530

    Update and rename TC_CSV.py to ClassDay2/TC_CSV.py

--- ClassDay2/TC_CSV.py
+++ ClassDay2/TC_CSV.py
@@ -4,4 +4,5 @@ with open("student.csv","w",newline="") as file:
     writer = csv.writer(file)
     writer.writerow(["Name", "ID", "Age"])
     writer.writerow(["Tena", "1", "20"])
+
     writer.writerow(["veena", "2", "30"])
