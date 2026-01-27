ae25165a192a2ff620e5f3e9bcad76b73577a3b4
MahendraChowdam <135975490+MahendraChowdam@users.noreply.github.com>
2026-01-27 18:39:42 +0530

    Add files via upload

---
+++ LabDay10/test_TC003_yield.py
@@ -0,0 +1,17 @@
+import pytest
+
+@pytest.mark.smoke
+def test_smoke1():
+    assert True
+
+@pytest.fixture
+def setup_teardown():
+    print("setup")
+    yield
+    print("teardown")
+
+def test_example(setup_teardown):
+    print("test running")
+
+def test_example1(setup_teardown):
+    print("test1 running")
