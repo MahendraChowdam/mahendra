3be5b374bd8f7f7c3a3525c3e0efd8a00e64d193
MahendraChowdam <135975490+MahendraChowdam@users.noreply.github.com>
2026-02-05 17:00:27 +0530

    Add files via upload

---
+++ TC005_Windowhandling.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+from selenium.webdriver.support.ui import WebDriverWait
+
+driver = webdriver.Chrome()
+driver.get("https://letcode.in/window")
+time.sleep(5)
+driver.find_element(By.ID,"multi").click()
+windows = driver.window_handles
+for window in windows:
+    driver.switch_to.window(window)
+    time.sleep(5)
+    print("title",driver.current_url)
