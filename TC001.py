0ef5b100266e2a686724dc807292d674994207c2
MahendraChowdam <135975490+MahendraChowdam@users.noreply.github.com>
2026-02-13 16:05:31 +0530

    Add files via upload

---
+++ TC001.py
@@ -0,0 +1,18 @@
+import mysql.connector
+host = "localhost"
+user = "root"
+password = "root"
+database = "feb2026"
+
+
+conn = mysql.connector.connect(host=host, user=user, password=password, database=database)
+cursor = conn.cursor()
+print("connect success")
+query="select * from feb2026.emp_table"
+cursor.execute(query)
+
+result = cursor.fetchall()
+
+for row in result:
+    print(row)
+
